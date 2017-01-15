0c3cd30cbd7602b5ead693c4e60150c1c4881baf
craigschiff <craig.schiff@gmail.com>
2017-01-15 10:55:28 -0500

    done

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,12 @@
+require 'pry'
+
 def alphabetize(arr)
   # code here
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  sorted_arr = arr.sort_by do |element|
+      element.chars.collect do |letter|
+          alphabet.index(letter)
+        end
+    end
+  sorted_arr
 end
